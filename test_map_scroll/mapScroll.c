fd45101c72f2457b3f930d3076798e3ecad25844
Alexandre <kanemaki@gmail.com>
2021-06-07 17:42:21 +0100

    chore: add template project

---
+++ test_map_scroll/mapScroll.c
@@ -0,0 +1,30 @@
+/*---------------------------------------------------------------------------------
+07/06/2021
+Alexandre Florentino
+
+kanemaki@gmail.com
+---------------------------------------------------------------------------------*/
+#include <snes.h>
+
+//---------------------------------------------------------------------------------
+int main(void) {
+    // Initialize SNES 
+	consoleInit();
+
+	// Put your init here
+	
+	// Now Put in 16 color mode 
+	setMode(BG_MODE1,0);  
+
+	// And let's show :P
+	setBrightness(0xF);  
+	
+	// Infinite loop to keep the program running
+	while(1) {
+		// Put your code here
+	
+		// Wait refresh
+		WaitForVBlank();
+	}
+	return 0;
+}
